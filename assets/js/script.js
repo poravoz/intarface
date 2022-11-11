0244a1e5b49f7c02c22e0c57f9784f70c00edc59
poravoz <116353344+poravoz@users.noreply.github.com>
2022-11-11 16:27:37 +0200

    Create script.js

---
+++ assets/js/script.js
@@ -0,0 +1,25 @@
+window.onload = function() {
+    $("#lr7").on("click", function() {
+       window.location.href = "https://poravoz.github.io/lr7/";
+    });
+
+    $("#lr8").on("click", function() {
+        window.location.href = "https://poravoz.github.io/lr8/";
+    });
+
+    $("#lr9").on("click", function() {
+        window.location.href = "https://poravoz.github.io/lr9/";
+    });
+
+    $("#lr10").on("click", function() {
+        window.location.href = "https://poravoz.github.io/lr10/";
+    });
+
+    $("#lr11").on("click", function() {
+        window.location.href = "https://poravoz.github.io/lr11/";
+    });
+
+    $("#lr12").on("click", function() {
+        window.location.href = "https://poravoz.github.io/lr12/";
+    });
+}
